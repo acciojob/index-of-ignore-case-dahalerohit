aabab2938594f995ae4fc0c25d7e4f7b9b1e5b92
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2024-10-06 09:03:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let s = s1.trim();
+	let str = s.indexof(" ");
+
+	if(str === -1)
+	{
+		return s
+	}
+	return s.substring(0,str);
+	
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+//const s1 = prompt("Enter s1:");
+//const s2 = prompt("Enter s2:");
+//alert(indexOfIgnoreCase(s1, s2));
